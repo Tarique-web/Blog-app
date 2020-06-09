ec00b8c1e102065d5ec665e4412e9e160e8a38f7
Tarique-web <52395663+Tarique-web@users.noreply.github.com>
2020-06-09 18:16:19 +0530

    Update blog_update.js

--- router/blog_update.js
+++ router/blog_update.js
@@ -3,17 +3,38 @@ module.exports = (router, knex,jwt) => {
     router
 
         .put("/update_blog/:id", (req, res) => {
-            // var cookie = req.headers.cookie;
-            // var token = cookie.slice(4);
-            // var token_verify = jwt.verify(token, "tarique");
-            // console.log(token_verify)
-            knex('post').where({"id":req.params.id}).update(req.body).then((result) => {
-                res.send("Updated succesfully!!")
-            })
-            .catch((err) => {
-                res.send(err)
-        })
-            // 
-        })
+           
+            // console.log(typeof(id_user));
+            
+            var cookie = req.headers.cookie.slice(4);            
+            var token_verify = jwt.verify(cookie, "tarique").first_name;            
 
+            knex('post').where({"user_id":token_verify}).then((results)=>{
+                id_user=(req.params.id)
+                // res.send(results)
+                for(var i of results){
+                    j=i.id
+                    if (j==id_user){  
+                        console.log("if working")                                              
+                        knex('post').where({"id":id_user}).update(req.body)
+                        .then((result) => {
+                            res.send("Updated succesfully!!")
+                   
+                        }).catch((err)=>{
+                            res.send(err)
+                        })
+                        if(j==id_user){
+                            break
+
+                        }
+                    }else{
+                        console.log('This blog is not written by you');
+                        // res.send("This Blog is not written bt you")
+                    }
+            }              
+            })
+            .catch((err)=>{
+                res.send(err)
+            }) 
+        })
 }
