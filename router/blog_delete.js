2d9c67e014ef32a4d89dce9a130190ed28b107cf
Tarique-web <52395663+Tarique-web@users.noreply.github.com>
2020-06-09 18:14:29 +0530

    Update blog_delete.js

--- router/blog_delete.js
+++ router/blog_delete.js
@@ -1,13 +1,40 @@
-module.exports = (router, knex) =>{
+module.exports = (router, knex,jwt) => {
 
     router
-        .delete('/blog_delete/:id', (req, res) => {
-            knex('post')
-                .where('id', req.params.id).del()
-                .then((result) => {
-                    res.send("blog deleted succesfully !!!")
 
-                })
-                    .catch((err) => { res.send(err) })
+        .delete("/delete_blog/:id", (req, res) => {
+           
+            // console.log(typeof(id_user));
+            
+            var cookie = req.headers.cookie.slice(4);            
+            var token_verify = jwt.verify(cookie, "tarique").first_name;            
+
+            knex('post').where({"user_id":token_verify}).then((results)=>{
+                id_user=(req.params.id)
+                // res.send(results)
+                for(var i of results){
+                    j=i.id
+                    if (j==id_user){  
+                        console.log("if working")                                              
+                        knex('post').where({"id":id_user}).del()
+                        .then((result) => {
+                            res.send("Blog delete succesfully!!")
+                   
+                        }).catch((err)=>{
+                            res.send(err)
+                        })
+                        if(j==id_user){
+                            break
+
+                        }
+                    }else{
+                        console.log('This blog is not written by you that\'s why you can\'t delete ');
+                        // res.send("This Blog is not written by you")
+                    }
+            }              
+            })
+            .catch((err)=>{
+                res.send(err)
+            }) 
         })
 }
